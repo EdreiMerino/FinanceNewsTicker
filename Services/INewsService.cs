8d5137f50de954d5e00b42407ab33d01e403e424
Edrei Merino <edreimerino@gmail.com>
2023-09-12 09:47:13 -0700

    Add project files.

---
+++ Services/INewsService.cs
@@ -0,0 +1,22 @@
+﻿namespace FinanceNewsTicker.Services
+{
+    public interface INewsService
+    {
+        void GetFinanceNews();
+    }
+
+    public class NewsService : INewsService
+    {
+        private readonly IConfiguration _configuration;
+
+        public NewsService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void GetFinanceNews()
+        {
+            string apiKey = _configuration.GetValue<string>("API_KEY");
+        }
+    }
+}
